6f7c3ee62d5f26d278b752d828ebd03dbba516c6
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2024-06-03 15:04:57 +0200

    start.c : make main extern

--- libfemto/arch/riscv/start.c
+++ libfemto/arch/riscv/start.c
@@ -7,7 +7,7 @@
 extern char _bss_end;
 extern char _memory_end;
 
-int main(int argc, char **argv);
+extern int main(int argc, char **argv);
 
 __attribute__((noreturn)) void libfemto_start_main()
 {
